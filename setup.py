df879946d651d38d030c849b8e479028cfc92a95
startskiller <53841222+startskiller@users.noreply.github.com>
2021-03-01 17:24:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="quantsio",
+    name="dseplayground",
     version="0.1.1",
     author="Pratik Gujarathi",
     description="Dummy Application to test custom import on sagemaker",
