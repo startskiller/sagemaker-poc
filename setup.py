1f5d27238a884c88c04e2ccebc59f751bc53eb13
startskiller <53841222+startskiller@users.noreply.github.com>
2021-03-01 17:24:13 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quantsio",
+    version="0.1.1",
+    author="Pratik Gujarathi",
+    description="Dummy Application to test custom import on sagemaker",
+    packages=find_packages()
+)
